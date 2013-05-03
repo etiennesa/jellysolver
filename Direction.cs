080a9737cd8563d04bd84efeaca16e02d267a4e4
etiennesa <etiennesanson@yahoo.fr>
2013-05-03 09:46:36 +0200

    new file Direction

---
+++ Direction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JellySolver
+{
+    class Direction
+    {
+        public int I { get; private set; }
+        public int J { get; private set; }
+
+        public Direction(int i, int j)
+        {
+            I = i;
+            J = j;
+        }
+
+        public static Direction LeftMove = new Direction(-1, 0);
+        public static Direction DownMove = new Direction(0, 1);
+        public static Direction RightMove = new Direction(1, 0);
+    }
+}
